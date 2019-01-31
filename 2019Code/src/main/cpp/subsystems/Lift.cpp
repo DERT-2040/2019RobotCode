6e06c79e81bde2c9a306b3f704c4d8dde2aaa9e3
Carter <carterdiorio@gmail.com>
2019-01-30 21:31:40 -0600

    Added different PID loops for four Bar and Lift to control a constant velocity

--- 2019Code/src/main/cpp/subsystems/Lift.cpp
+++ 2019Code/src/main/cpp/subsystems/Lift.cpp
@@ -13,10 +13,28 @@ Lift::Lift() : Subsystem("Lift")
   talonTimeoutMs = 30;
   liftCruiseVelocity = 10000;
   liftAcceleration = 5000;
-  liftkP = 0;
-  liftkI = 0;
-  liftkD = 0;
-  liftkF = 0;
+  fourBarCruiseVelocity = 10000;
+  fourBarAcceleration = 5000;
+
+  mLiftkP = 0;
+  mLiftkI = 0;
+  mLiftkD = 0;
+  mLiftkF = 0;
+
+  vLiftkP = 0;
+  vLiftkI = 0;
+  vLiftkD = 0;
+  vLiftkF = 0;
+
+  mFourBarkP = 0;
+  mFourBarkI = 0;
+  mFourBarkD = 0;
+  mFourBarkP = 0;
+
+  vFourBarkP = 0;
+  vFourBarkI = 0;
+  vFourBarkD = 0;
+  vFourBarkF = 0;
 
   masterLiftMotor = new WPI_TalonSRX(kMasterLiftMotorPort);
   masterLiftMotor->SetInverted(false);
@@ -26,12 +44,19 @@ Lift::Lift() : Subsystem("Lift")
   masterLiftMotor->SetSensorPhase(true);
   masterLiftMotor->SetSelectedSensorPosition(0, kPIDLoopIdx, talonTimeoutMs);
 
-  masterLiftMotor->Config_kP(kSlotIdx, liftkP, talonTimeoutMs);
-  masterLiftMotor->Config_kI(kSlotIdx, liftkI, talonTimeoutMs);
-  masterLiftMotor->Config_kD(kSlotIdx, liftkD, talonTimeoutMs);
-  masterLiftMotor->Config_kF(kSlotIdx, liftkF, talonTimeoutMs);
+  masterLiftMotor->Config_kP(kElevatorMotionSlotIdx, mLiftkP, talonTimeoutMs);
+  masterLiftMotor->Config_kI(kElevatorMotionSlotIdx, mLiftkI, talonTimeoutMs);
+  masterLiftMotor->Config_kD(kElevatorMotionSlotIdx, mLiftkD, talonTimeoutMs);
+  masterLiftMotor->Config_kF(kElevatorMotionSlotIdx, mLiftkF, talonTimeoutMs);
+
+  masterLiftMotor->Config_kP(kElevatorVelocitySlotIdx, vLiftkP, talonTimeoutMs);
+  masterLiftMotor->Config_kI(kElevatorVelocitySlotIdx, vLiftkI, talonTimeoutMs);
+  masterLiftMotor->Config_kD(kElevatorVelocitySlotIdx, vLiftkD, talonTimeoutMs);
+  masterLiftMotor->Config_kF(kElevatorVelocitySlotIdx, vLiftkF, talonTimeoutMs);
+
   masterLiftMotor->ConfigMotionCruiseVelocity(liftCruiseVelocity);
   masterLiftMotor->ConfigMotionCruiseVelocity(liftAcceleration);
+  masterLiftMotor->ConfigAllowableClosedloopError(kElevatorMotionSlotIdx, liftPIDError, talonTimeoutMs);
 
   secondLiftMotor = new WPI_TalonSRX(kSecondLiftMotorPort);
   secondLiftMotor->SetInverted(false);
@@ -41,13 +66,20 @@ Lift::Lift() : Subsystem("Lift")
   fourBarMotor->SetInverted(false);
   fourBarMotor->ConfigSelectedFeedbackSensor(FeedbackDevice::Analog, kPIDLoopIdx, talonTimeoutMs);
   
-  fourBarMotor->Config_kP(kSlotIdx, fbkP, talonTimeoutMs);
-  fourBarMotor->Config_kI(kSlotIdx, fbkI, talonTimeoutMs);
-  fourBarMotor->Config_kD(kSlotIdx, fbkD, talonTimeoutMs);
-  fourBarMotor->Config_kF(kSlotIdx, fbkF, talonTimeoutMs);
+  fourBarMotor->Config_kP(kFourBarMotionSlotIdx, mFourBarkP, talonTimeoutMs);
+  fourBarMotor->Config_kI(kFourBarMotionSlotIdx, mFourBarkI, talonTimeoutMs);
+  fourBarMotor->Config_kD(kFourBarMotionSlotIdx, mFourBarkD, talonTimeoutMs);
+  fourBarMotor->Config_kF(kFourBarMotionSlotIdx, mFourBarkF, talonTimeoutMs);
+
+  fourBarMotor->Config_kP(kFourBarVelocitySlotIdx, vFourBarkP, talonTimeoutMs);
+  fourBarMotor->Config_kI(kFourBarVelocitySlotIdx, vFourBarkI, talonTimeoutMs);
+  fourBarMotor->Config_kD(kFourBarVelocitySlotIdx, vFourBarkD, talonTimeoutMs);
+  fourBarMotor->Config_kF(kFourBarVelocitySlotIdx, vFourBarkF, talonTimeoutMs);
+
   fourBarMotor->ConfigMotionCruiseVelocity(fourBarCruiseVelocity);
   fourBarMotor->ConfigMotionAcceleration(fourBarAcceleration);
-
+  fourBarMotor->ConfigAllowableClosedloopError(kFourBarMotionSlotIdx, fourBarPIDError, talonTimeoutMs);
+  
 }
 
 void Lift::InitDefaultCommand() 
@@ -57,13 +89,20 @@ void Lift::InitDefaultCommand()
 
 void Lift::setElevatorHeight(double height)
 {
+  masterLiftMotor->SelectProfileSlot(kElevatorMotionSlotIdx, kPIDLoopIdx);
   masterLiftMotor->Set(ControlMode::MotionMagic, height*heightToTickRatio);
 }
 
 void Lift::setFourBarHeight(double height)
 {
   double angle = asin(height/fourBarLength) * 180 / M_PI;
-
 }
+
+void Lift::joystickElevatorControl(double speed)
+{
+  masterLiftMotor->SelectProfileSlot(kElevatorVelocitySlotIdx, kPIDLoopIdx);
+  masterLiftMotor->Set(ControlMode::Velocity, speed*maxLiftSpeed);
+}
+
 
 
--- 2019Code/src/main/include/subsystems/Lift.h
+++ 2019Code/src/main/include/subsystems/Lift.h
@@ -23,17 +23,33 @@ class Lift : public frc::Subsystem
   WPI_TalonSRX * fourBarMotor;
 
   double talonTimeoutMs;
-  double liftkP, liftkI, liftkD, liftkF;
-  double fbkP, fbkI, fbkD, fbkF;
+  double mLiftkP, mLiftkI, mLiftkD, mLiftkF;
+  double vLiftkP, vLiftkI, vLiftkD, vLiftkF;
+
+  double mFourBarkP, mFourBarkI, mFourBarkD, mFourBarkF;
+  double vFourBarkP, vFourBarkI, vFourBarkD, vFourBarkF;
+
   double liftCruiseVelocity, liftAcceleration;
   double fourBarCruiseVelocity, fourBarAcceleration;
 
   //constants
   const int kPIDLoopIdx = 0;
-  const int kSlotIdx = 0;
+
+  const int kElevatorMotionSlotIdx = 0;
+  const int kElevatorVelocitySlotIdx = 1;
+
+  const int kFourBarMotionSlotIdx = 0;
+  const int kFourBarVelocitySlotIdx = 1;
+
   const double heightToTickRatio = 0;
   const double angleToUnitRatio = 0;
   const double fourBarLength = 0;
+  const double maxLiftSpeed = 10000;
+
+  const double liftPIDError = .01;
+  const double fourBarPIDError = .01;
+
+
 
  public:
 
@@ -41,4 +57,5 @@ class Lift : public frc::Subsystem
   void InitDefaultCommand() override;
   void setElevatorHeight(double height);
   void setFourBarHeight(double height);
+  void joystickElevatorControl(double speed);
 };
